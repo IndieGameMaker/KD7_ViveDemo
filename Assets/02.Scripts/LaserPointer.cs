94eecc2a0626108e9a17be066713d90e486ed6b9
Lee Jae Hyun <myleje@gmail.com>
2021-09-23 14:50:48 +0900

    .

--- Assets/02.Scripts/LaserPointer.cs
+++ Assets/02.Scripts/LaserPointer.cs
@@ -10,12 +10,27 @@ public class LaserPointer : MonoBehaviour
     private LineRenderer line;
     private Transform tr;
 
+    [Range(5.0f,30.0f)]
+    public float maxDistance = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         pose = GetComponent<SteamVR_Behaviour_Pose>();
         hand = pose.inputSource;
         tr = GetComponent<Transform>();
+        CreateLine();
+    }
+
+    void CreateLine()
+    {
+        line = this.gameObject.AddComponent<LineRenderer>();
+
+        line.useWorldSpace = false;
+
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, )
     }
 
     // Update is called once per frame
