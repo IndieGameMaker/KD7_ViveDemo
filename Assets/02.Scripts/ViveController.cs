76595a5e23a85a038b456dfb5d0f467abf09986c
Lee Jae Hyun <myleje@gmail.com>
2021-09-23 13:31:58 +0900

    Project Upload

---
+++ Assets/02.Scripts/ViveController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+    public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Input_Sources any = SteamVR_Input_Sources.Any;
+
+    public SteamVR_Action_Boolean trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = SteamVR_Actions.default_InteractUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (trigger.GetStateDown(leftHand))
+        {
+            Debug.Log("Left Trigger Down");
+        }
+
+        if (trigger.GetStateDown(rightHand))
+        {
+            Debug.Log("Right Trigger Down");
+        }
+        
+    }
+}
